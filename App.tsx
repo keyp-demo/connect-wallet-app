787c2c989cb9566a71de197eae6fdb12a930f6c1
loveyoubaby5120 <kaipan.1314.666@gmail.com>
2022-02-15 18:25:07 +0800

    feat: connect wallet

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { withWalletConnect } from '@walletconnect/react-native-dapp';
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+const { scheme } = require('expo');
+import { Platform } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
-export default function App() {
+function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -20,3 +25,10 @@ export default function App() {
     );
   }
 }
+
+export default withWalletConnect(App, {
+  redirectUrl: Platform.OS === 'web' ? window.location.origin : `${scheme}://`,
+  storageOptions: {
+    asyncStorage: AsyncStorage as any,
+  },
+})
--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -1,79 +1,42 @@
-import * as WebBrowser from 'expo-web-browser';
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import { useWalletConnect } from '@walletconnect/react-native-dapp';
+import WalletConnect from "@walletconnect/client";
+import React from 'react';
+import { Alert, Button, TouchableHighlight } from 'react-native';
 
-import Colors from '../constants/Colors';
-import { MonoText } from './StyledText';
 import { Text, View } from './Themed';
+import { ethers } from 'ethers';
 
 export default function EditScreenInfo({ path }: { path: string }) {
+  // return (
+  //   <View>
+  //     <Text>12222</Text>
+  //     </View>
+  // );
+  const connector = useWalletConnect();
+  if (!connector.connected) {
+    return <Button title="Connect" onPress={() => connector.connect()} />
+  }
+
   return (
     <View>
-      <View style={styles.getStartedContainer}>
-        <Text
-          style={styles.getStartedText}
-          lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
-          Open up the code for this screen:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
-          darkColor="rgba(255,255,255,0.05)"
-          lightColor="rgba(0,0,0,0.05)">
-          <MonoText>{path}</MonoText>
-        </View>
-
-        <Text
-          style={styles.getStartedText}
-          lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
-          Change any of the text, save the file, and your app will automatically update.
-        </Text>
-      </View>
-
-      <View style={styles.helpContainer}>
-        <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-          <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
-            Tap here if your app doesn't automatically update after making changes
-          </Text>
-        </TouchableOpacity>
-      </View>
+      <TouchableHighlight
+        style={{
+          marginBottom: 20,
+        }}>
+        <Button title="signer" onPress={async () => await signer(connector)} />
+      </TouchableHighlight>
+      <Button title="Kill Session" onPress={() => connector.killSession()} />
     </View>
   );
 }
 
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet'
-  );
+async function signer(connector: WalletConnect) {
+  const userId = "dIPs1srYFLNOhyptxgdxO9KBJm33";
+  const timestamp = new Date().getTime();
+  // const timestamp = 1644583981945;
+  const messageInfo = `This information is only used to verify user: ${userId} is the owner of address ${connector.accounts[0]}. It is valid before ${timestamp}`;
+  const messageBytes = ethers.utils.toUtf8Bytes(messageInfo);
+  const signature = await connector.signPersonalMessage([messageBytes, connector.accounts[0]]);
+  console.log(signature, timestamp);
+  Alert.alert('提示', signature);
 }
-
-const styles = StyleSheet.create({
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightContainer: {
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  helpContainer: {
-    marginTop: 15,
-    marginHorizontal: 20,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    textAlign: 'center',
-  },
-});
